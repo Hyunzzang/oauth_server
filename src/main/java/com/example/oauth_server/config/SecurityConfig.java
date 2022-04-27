a092a4f76674a3762fecbf8f9c507cbf16a0afd6
hyunchul <phc023@lfcorp.com>
2022-04-27 18:26:33 +0900

    ..

--- src/main/java/com/example/oauth_server/config/SecurityConfig.java
+++ src/main/java/com/example/oauth_server/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.oauth_server.security.oauth2.*;
 import com.example.oauth_server.security.oauth2.service.CustomOAuth2UserService;
 import com.example.oauth_server.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -18,6 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.savedrequest.CookieRequestCache;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -35,6 +37,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final TokenAuthenticationFilter tokenAuthenticationFilter;
     private final AuthenticationSuccessHandler authenticationSuccessHandler;
+
+    @Value("${spring.security.oauth2.resourceserver.opaquetoken.introspection-uri}")
+    private String introspectionUri;
+
+    @Value("${spring.security.oauth2.resourceserver.opaquetoken.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.resourceserver.opaquetoken.client-secret}")
+    private String clientSecret;
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -59,14 +70,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // Endpoint protection
                 .authorizeHttpRequests(config -> {
                     config.antMatchers("/oauth/**", "/oauth2/**", "/h2-console/**").permitAll();
-                    config.antMatchers("/join", "/oauth2_join/**", "/revoke_token", "/custom_login").permitAll();
+                    config.antMatchers("/join", "/oauth2_join/**", "/revoke_token", "/custom_login", "/api/test/home").permitAll();
                     config.anyRequest().authenticated();
                 })
+                .httpBasic(config -> config.and())
                 .formLogin(config -> {
 //                    config.loginPage("/custom_login");
                     config.successHandler(authenticationSuccessHandler);
                 })
-                .httpBasic(config -> config.and())
 //                .requestCache(config -> {
 //                    config.requestCache(new CookieRequestCache());
 //                })
@@ -94,8 +105,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                    config.defaultSuccessUrl("/loginSuccess");
 //                    config.failureUrl("/loginFailure");
                 })
+                // 권한이 필요한 uri 접근시 oauth2ResourceServer 또는 addFilterBefore 둘중 하나로 처리
+                .oauth2ResourceServer(oauth2ResourceServer -> {
+                    oauth2ResourceServer.opaqueToken(token -> token.introspectionUri(introspectionUri)
+                            .introspectionClientCredentials(clientId, clientSecret));
+                })
                 // Filters
-                .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+//                .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 // Auth exceptions
                 .exceptionHandling(config -> {
                     config.accessDeniedHandler(this::accessDenied);
